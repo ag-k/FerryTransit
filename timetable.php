0b8e2375464827d567cdf65dd4b8357873aa776e
ag <ag.aeg.age@gmail.com>
2016-05-24 16:32:04 +0900

    クロスドメインアクセスの実験

--- timetable.php
+++ timetable.php
@@ -2,6 +2,7 @@
 try {
     $db = new PDO('sqlite:timetable.sqlite');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    header('Access-Control-Allow-Origin: *');
     echo json_encode($db->query('SELECT * FROM timetable')->fetchAll(PDO::FETCH_ASSOC));
 } catch (PDOException $e) {
     die ('Connection failed : '.$e->getMessage());
