34d58911a6a1dfa340db112fb6bea0f7ba9d18a7
ag <ag.aeg.age@gmail.com>
2016-12-26 21:50:14 +0900

    Update timetable

--- update_timetable.sql
+++ update_timetable.sql
@@ -1,7 +1,8 @@
-drop table timetable;
+/* drop table timetable;
 vacuum;
 create table timetable(trip_id, next_id,  start_date, end_date, name, departure, departure_time, arrival, arrival_time);
+*/
 .separator ","
-.import timetable-2016-result_oki-kisen.csv timetable
-.import timetable-2016-result_oki-kanko.csv timetable
+.import timetable-2017-result_oki-kisen.csv timetable
+.import timetable-2017-result_oki-kanko.csv timetable
 
