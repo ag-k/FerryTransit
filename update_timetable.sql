7ca3716e0078b13588fc90008e5f3e84a78dd723
Koyama Eiji <ag.aeg.age@gmail.com>
2017-12-20 12:10:10 +0900

    2018年版の時刻表に更新

--- update_timetable.sql
+++ update_timetable.sql
@@ -1,8 +1,9 @@
-/* drop table timetable;
+drop table timetable;
 vacuum;
 create table timetable(trip_id, next_id,  start_date, end_date, name, departure, departure_time, arrival, arrival_time);
-*/
+
 .separator ","
 .import timetable-2017-result_oki-kisen.csv timetable
 .import timetable-2017-result_oki-kanko.csv timetable
-
+.import timetable-2018-result_oki-kisen.csv timetable
+.import timetable-2018-result_oki-kanko.csv timetable
