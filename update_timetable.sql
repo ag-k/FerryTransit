75ebc846a542a039f40987711d2d8b6f21f74c2c
Koyama Eiji <ag.aeg.age@gmail.com>
2018-11-24 00:10:14 +0900

    隠岐汽船時刻表を2019年版に改定

--- update_timetable.sql
+++ update_timetable.sql
@@ -3,7 +3,5 @@ vacuum;
 create table timetable(trip_id, next_id,  start_date, end_date, name, departure, departure_time, arrival, arrival_time);
 
 .separator ","
-.import timetable-2017-result_oki-kisen.csv timetable
-.import timetable-2017-result_oki-kanko.csv timetable
-.import timetable-2018-result_oki-kisen.csv timetable
-.import timetable-2018-result_oki-kanko.csv timetable
+.import timetable-2019v1-result_oki-kisen.csv timetable
+.import timetable-2019v1-result_oki-kanko.csv timetable
