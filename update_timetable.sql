cfb07e07183e357827a2c01ee6e2fe3c155c78f0
Koyama Eiji <ag.aeg.age@gmail.com>
2020-04-25 08:43:58 +0900

    フェリーの臨時ダイヤ更新

--- update_timetable.sql
+++ update_timetable.sql
@@ -3,5 +3,5 @@ vacuum;
 create table timetable(trip_id, next_id,  start_date, end_date, name, departure, departure_time, arrival, arrival_time);
 
 .separator ","
-.import timetable-2020v2-result_oki-kisen.csv timetable
-.import timetable-2020v2-result_oki-kanko.csv timetable
+.import timetable-2020v4-result_oki-kisen.csv timetable
+.import timetable-2020v4-result_oki-kanko.csv timetable
