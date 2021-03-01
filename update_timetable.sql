a5c6bf08d1b05953bd08398a133fb90ab20bab38
Koyama Eiji <ag.aeg.age@gmail.com>
2021-03-01 11:08:40 +0900

    高速船の時刻表を修正

--- update_timetable.sql
+++ update_timetable.sql
@@ -3,5 +3,5 @@ vacuum;
 create table timetable(trip_id, next_id,  start_date, end_date, name, departure, departure_time, arrival, arrival_time);
 
 .separator ","
-.import timetable-2021-result_oki-kisen.csv timetable
-.import timetable-2021-result_oki-kanko.csv timetable
+.import timetable-2021v2-result_oki-kisen.csv timetable
+.import timetable-2021v2-result_oki-kanko.csv timetable
