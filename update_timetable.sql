b98333e8ceb64a83df492d17d5508a4fdfbd726c
Koyama Eiji <ag.aeg.age@gmail.com>
2021-09-07 07:14:05 +0900

    時刻表更新

--- update_timetable.sql
+++ update_timetable.sql
@@ -1,7 +1,18 @@
 drop table timetable;
 vacuum;
-create table timetable(trip_id, next_id,  start_date, end_date, name, departure, departure_time, arrival, arrival_time);
+create table timetable
+(
+    trip_id,
+    next_id,
+    start_date,
+    end_date,
+    name,
+    departure,
+    departure_time,
+    arrival,
+    arrival_time
+);
 
 .separator ","
-.import timetable-2021v3-result_oki-kisen.csv timetable
-.import timetable-2021v2-result_oki-kanko.csv timetable
+.import timetable-2021v4-result_oki-kisen.csv timetable
+.import timetable-2021v4-result_oki-kanko.csv timetable
