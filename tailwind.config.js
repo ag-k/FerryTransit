2528627f1c6f50f95ba6293d0c5a49fc9bd68b2b
Koyama Eiji <ag.aeg.age@gmail.com>
2026-01-11 14:45:12 +0900

    style(ui): align fonts and tab styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,8 +33,8 @@ export default {
       },
       fontFamily: {
         sans: [
+          '"Inter"',
           '"BIZ UDPGothic"',
-          '"Noto Sans JP"',
           'system-ui',
           '-apple-system',
           '"Segoe UI"',
