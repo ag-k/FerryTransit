aaa33d1c416972d746da04403ba828fcd998b134
Koyama Eiji <ag.aeg.age@gmail.com>
2026-02-20 04:08:20 +0900

    refactor(functions): remove unused getTimetableFromStorage

--- src/functions/src/index.ts
+++ src/functions/src/index.ts
@@ -1,4 +1,4 @@
-import { getTimetable, getTimetableFromStorage } from "./timetable";
+import { getTimetable } from "./timetable";
 import { ensureAdminApp } from "./utils/adminApp";
 
 // Initialize Firebase Admin (lazy / Secret Manager対応)
@@ -7,7 +7,6 @@ ensureAdminApp();
 // Export functions
 export {
   getTimetable as getTimetableData,
-  getTimetableFromStorage as getTimetableStorage,
 };
 
 // Export admin functions
